4694e02295edfe2e0a6ac84ff9794f6fc1881b1f
Titus Wormer <tituswormer@gmail.com>
2022-11-01 14:33:12 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,50 +1,52 @@
-import test from 'tape'
+import assert from 'node:assert'
+import test from 'node:test'
 import {daleChallFormula, daleChallGradeLevel} from './index.js'
 
-test('daleChallFormula', function (t) {
-  // @ts-ignore runtime
-  t.ok(Number.isNaN(daleChallFormula()), 'NaN when an invalid value is given')
-  t.equal(round(daleChallFormula({word: 30, sentence: 2})), 0.744)
-  t.equal(
+test('daleChallFormula', function () {
+  assert.ok(
+    // @ts-expect-error runtime
+    Number.isNaN(daleChallFormula()),
+    'NaN when an invalid value is given'
+  )
+  assert.equal(round(daleChallFormula({word: 30, sentence: 2})), 0.744)
+  assert.equal(
     round(daleChallFormula({word: 30, sentence: 2, difficultWord: 6})),
     7.5385
   )
-  t.equal(
+  assert.equal(
     round(daleChallFormula({word: 30, sentence: 1, difficultWord: 12})),
     11.4405
   )
-  t.end()
 })
 
 // Grade-level.
-test('daleChallGradeLevel(score)', function (t) {
-  t.ok(
-    // @ts-ignore runtime
+test('daleChallGradeLevel(score)', function () {
+  assert.ok(
+    // @ts-expect-error runtime
     Number.isNaN(daleChallGradeLevel(daleChallFormula())[0]),
     'NaN when an invalid value is given'
   )
-  t.ok(
-    // @ts-ignore runtime
+  assert.ok(
+    // @ts-expect-error runtime
     Number.isNaN(daleChallGradeLevel(daleChallFormula())[1]),
     'NaN when an invalid value is given'
   )
-  t.deepEqual(
+  assert.deepEqual(
     daleChallGradeLevel(daleChallFormula({word: 30, sentence: 2})),
     [0, 4]
   )
-  t.deepEqual(
+  assert.deepEqual(
     daleChallGradeLevel(
       daleChallFormula({word: 30, sentence: 2, difficultWord: 6})
     ),
     [9, 10]
   )
-  t.deepEqual(
+  assert.deepEqual(
     daleChallGradeLevel(
       daleChallFormula({word: 30, sentence: 1, difficultWord: 12})
     ),
     [16, Number.POSITIVE_INFINITY]
   )
-  t.end()
 })
 
 /**
